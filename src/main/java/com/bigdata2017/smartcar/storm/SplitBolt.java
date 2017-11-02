fc256d1c4cf344bd33593da9b6651fa644252ae6
kickscar-github <kickscar@gmail.com>
2017-11-03 04:29:31 +0900

    log 추가

--- src/main/java/com/bigdata2017/smartcar/storm/SplitBolt.java
+++ src/main/java/com/bigdata2017/smartcar/storm/SplitBolt.java
@@ -23,9 +23,9 @@ public class SplitBolt extends BaseBasicBolt {
 		String tValue = tuple.getString(0);  
 		String[] receiveData = tValue.split("\\,");
 		
-		LOGGER.error(Arrays.toString(receiveData));
+		LOGGER.info(Arrays.toString(receiveData));
 		
-		//발생일시(14자리), 차량번호, 가속페달, 브레이크페달, 운전대회적각, 방향지시등, 주행속도, 뮤직번호
+		//발생일시(14자리), 차량번호, 가속페달, 브레이크페달, 운전대회적각, 방향지시등, 주행속도, 주행지역
 		collector.emit( new Values( new StringBuffer( receiveData[0] ).reverse() + "-" + receiveData[1],
 									receiveData[0],
 									receiveData[1],
