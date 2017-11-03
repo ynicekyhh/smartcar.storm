9b78cff063427eb48d2b2e8cb79fff7c33c39746
kickscar-github <kickscar@gmail.com>
2017-11-03 20:23:42 +0900

    수정

--- src/main/java/com/bigdata2017/smartcar/storm/HBaseBolt.java
+++ src/main/java/com/bigdata2017/smartcar/storm/HBaseBolt.java
@@ -19,17 +19,23 @@ import backtype.storm.tuple.Tuple;
 @SuppressWarnings("serial")
 public class HBaseBolt implements IRichBolt {
 
-	protected OutputCollector collector;
-	protected HTableConnector connector;
-	protected TupleTableConfig tupleTableConfig;
-	protected boolean autoAck = true;
+	private static final String TABLE_NAME = "table_smartcar_driving";
+	private static final String ZOOKEEPER_QUORUM = "server02.hadoop.com";
+	private static final String ZOOKEEPER_CLIENT_PORT = "2181";
 
-	public HBaseBolt() {
-		tupleTableConfig = new TupleTableConfig();
-	}
+	private static final String TUPLE_ROW_KEY_FIELD = "r_key";		
+	private static final String COLUMN_FAMILY = "cf";
+//	private static final String TUPLE_TIMESTAMP_FIELD = "";
+	private static final boolean IS_BATCH = false;
+	
+	private static final String[] COLUMN_NAMES = { "date", "car_number", "speed_pedal", "break_pedal", "steer_angle", "direct_light", "speed", "area_number" };
+	
+	private OutputCollector collector;
+	private HTableConnector connector;
+	private boolean autoAck = true;
 
-	@SuppressWarnings("rawtypes")	
 	@Override
+	@SuppressWarnings( "rawtypes" )	
 	public void prepare(Map map, TopologyContext context, OutputCollector collector) {
 		this.collector = collector;
 
@@ -43,20 +49,20 @@ public class HBaseBolt implements IRichBolt {
 	@Override
 	public void execute(Tuple tuple) {
 		try {
-			this.connector.getTable().put( TupleTableConfig.getPutFromTuple( tuple ) );
+			connector.getTable().put( getPutFromTuple( tuple ) );
 		} catch (IOException ex) {
 			throw new RuntimeException(ex);
 		}
 
-		if (this.autoAck) {
-			this.collector.ack(tuple);
+		if( autoAck ) {
+			collector.ack( tuple );
 		}		
 	}
 
 
 	@Override
 	public void cleanup() {
-		this.connector.close();		
+		connector.close();		
 	}
 	
 	@Override
@@ -69,6 +75,31 @@ public class HBaseBolt implements IRichBolt {
 		return null;
 	}
 	
+	private Put getPutFromTuple(final Tuple tuple) {
+		long ts = 0;
+		byte[] rowKey = Bytes.toBytes( tuple.getStringByField( TUPLE_ROW_KEY_FIELD ) );
+//		if (!TUPLE_TIMESTAMP_FIELD.equals( "" )) {
+//			ts = tuple.getLongByField( TUPLE_TIMESTAMP_FIELD );
+//		}
+
+		Put p = new Put( rowKey );
+		p.setWriteToWAL(true);
+
+		byte[] cfBytes = Bytes.toBytes( COLUMN_FAMILY );
+		for( String cq : COLUMN_NAMES ) {
+			byte[] cqBytes = Bytes.toBytes( cq );
+			byte[] val = Bytes.toBytes( tuple.getStringByField( cq ) );
+			if (ts > 0) {
+				p.add( cfBytes, cqBytes, ts, val );
+			} else {
+				p.add( cfBytes, cqBytes, val );
+			}
+		}
+
+		return p;
+	}
+
+	
 	private class HTableConnector implements Serializable {
 
 		private HTable hTable;
@@ -76,22 +107,22 @@ public class HBaseBolt implements IRichBolt {
 		public HTableConnector() throws IOException {
 			try {
 				Configuration config = HBaseConfiguration.create();
-				config.set( "hbase.zookeeper.quorum", TupleTableConfig.ZOOKEEPER_QUORUM );
-				config.set( "hbase.zookeeper.property.clientPort", TupleTableConfig.ZOOKEEPER_CLIENT_PORT );
+				config.set( "hbase.zookeeper.quorum", ZOOKEEPER_QUORUM );
+				config.set( "hbase.zookeeper.property.clientPort", ZOOKEEPER_CLIENT_PORT );
 				config.set( "hbase.cluster.distributed", "true" );
 	
-				hTable = new HTable( config, TupleTableConfig.TABLE_NAME );
+				hTable = new HTable( config, TABLE_NAME );
 	
-				if ( TupleTableConfig.IS_BATCH ) {
+				if ( IS_BATCH ) {
 					hTable.setAutoFlush( false, true );
 				}
 	
 				// Check the configured column families exist
-				if ( !columnFamilyExists( TupleTableConfig.COLUMN_FAMILY ) ) {
-					throw new RuntimeException(String.format("HBase table '%s' does not have column family '%s'", TupleTableConfig.TABLE_NAME, TupleTableConfig.COLUMN_FAMILY ));
+				if ( !columnFamilyExists( COLUMN_FAMILY ) ) {
+					throw new RuntimeException( String.format("HBase table '%s' does not have column family '%s'", TABLE_NAME, COLUMN_FAMILY ));
 				}
 			} catch (IOException ex) {
-				throw new IOException( "Unable to establish connection to HBase table "	+ TupleTableConfig.TABLE_NAME, ex);
+				throw new IOException( "Unable to establish connection to HBase table "	+ TABLE_NAME, ex );
 			}
 		}
 
@@ -113,53 +144,4 @@ public class HBaseBolt implements IRichBolt {
 			}
 		}
 	}
-	
-	private static class TupleTableConfig implements Serializable {
-		static {
-			TupleTableConfig.COLUMN_NAMES = new String[]{ 
-				"date", 
-				"car_number",
-				"speed_pedal",
-				"break_pedal",
-				"steer_angle",
-				"direct_light",
-				"speed",
-				"area_number" };
-		}
-		
-		private static final String TABLE_NAME = "table_smartcar_driving";
-		private static final String ZOOKEEPER_QUORUM = "server02.hadoop.com";
-		private static final String ZOOKEEPER_CLIENT_PORT = "2181";
-
-		private static final String TUPLE_ROW_KEY_FIELD = "r_key";		
-		private static final String COLUMN_FAMILY = "cf";
-//		private static final String TUPLE_TIMESTAMP_FIELD = "";
-		private static final boolean IS_BATCH = false;
-
-		private static String[] COLUMN_NAMES = null;
-		
-		public static Put getPutFromTuple(final Tuple tuple) {
-			long ts = 0;
-			byte[] rowKey = Bytes.toBytes( tuple.getStringByField( TUPLE_ROW_KEY_FIELD ) );
-//			if (!TUPLE_TIMESTAMP_FIELD.equals( "" )) {
-//				ts = tuple.getLongByField( TUPLE_TIMESTAMP_FIELD );
-//			}
-
-			Put p = new Put( rowKey );
-			p.setWriteToWAL(true);
-
-			byte[] cfBytes = Bytes.toBytes( COLUMN_FAMILY );
-			for( String cq : COLUMN_NAMES ) {
-				byte[] cqBytes = Bytes.toBytes( cq );
-				byte[] val = Bytes.toBytes( tuple.getStringByField( cq ) );
-				if (ts > 0) {
-					p.add( cfBytes, cqBytes, ts, val );
-				} else {
-					p.add( cfBytes, cqBytes, val );
-				}
-			}
-
-			return p;
-		}
-	}	
 }
